5e32dcc608bd7beab353775fc42346f07bc8f870
bvanvugt <1531419+bvanvugt@users.noreply.github.com>
2023-02-14 21:49:17 +0000

    Support running commands in dev containers

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 go install github.com/bvanvugt/cmd@latest
 ```
 
-
+Example Config:
 ```yaml
 # .devcontainer/cmd.yaml
 
@@ -15,4 +15,24 @@ env:
 commands:
   run:
     shell: while true; do echo 'barf?'; sleep 1; done
+```
+
+Dev Config:
+```yaml
+devcontainer:
+  name: cmd.local
+  dir: /workspaces/cmd
+env:
+  EXAMPLE: value
+commands:
+  install:
+    shell: go install .
+  run:
+    shell: go run cmd.go barf
+  barf:
+    shell: while true; do echo 'barf?'; sleep 1; done
+  test:
+    shell: echo test!!!
+  pwd:
+    shell: pwd
 ```
--- cmd.go
+++ cmd.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -13,9 +15,14 @@ import (
 	"github.com/spf13/viper"
 )
 
+var devContainerCmd string
+
+var systemOut, systemErr *color.Color
+
 type CmdConfig struct {
-	Env              map[string]string
 	DevContainerName string
+	DevContainerDir  string
+	Env              map[string]string
 	Commands         map[string]*CmdCommand
 }
 
@@ -24,62 +31,94 @@ type CmdCommand struct {
 	Shell string
 }
 
-func main() {
+func (cc *CmdCommand) Run(_ *cobra.Command, args []string) {
 	ctx := context.Background()
 
-	config := initConfig()
-	systemOut := color.New(color.FgBlack)
+	startTime := time.Now()
+
+	cmd := exec.CommandContext(ctx, "bash", "-c", fmt.Sprintf("%s %s", devContainerCmd, cc.Shell))
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		s := <-c
+
+		systemOut.Printf("\nReceived signal: %s\n", s)
+		err := cmd.Process.Signal(s)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	systemOut.Printf("Running [%s] at %s\n", cc.Name, startTime.Format(time.Kitchen))
+	cmd.Run()
+
+	endTime := time.Now()
+	systemOut.Printf("Completed [%s] at %s after %s\n", cc.Name, endTime.Format(time.Kitchen), endTime.Sub(startTime))
+}
+
+func init() {
+	systemOut = color.New(color.FgHiBlack)
+	systemErr = color.New(color.FgHiRed)
+}
+
+func main() {
+
+	config := loadConfig()
 
 	rootCmd := &cobra.Command{
 		Use: "cmd",
 	}
 
+	devCmd := &cobra.Command{
+		Use: "dev",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			devContainerCmd = fmt.Sprintf("docker exec -it -w %s %s", config.DevContainerDir, config.DevContainerName)
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			systemErr.Println("no command name provided")
+		},
+	}
+
 	for n, c := range config.Commands {
+		devCmd.AddCommand(&cobra.Command{
+			Use: n,
+			Run: c.Run,
+		})
 		rootCmd.AddCommand(&cobra.Command{
 			Use: n,
-			Run: func(_ *cobra.Command, args []string) {
-				startTime := time.Now()
-
-				cmd := exec.CommandContext(ctx, "bash", "-c", c.Shell)
-
-				cmd.Stdout = os.Stdout
-				cmd.Stderr = os.Stderr
-				cmd.Stdin = os.Stdin
-
-				go func() {
-					c := make(chan os.Signal, 1)
-					signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
-					s := <-c
-
-					systemOut.Printf("\nReceived signal: %s\n", s)
-					err := cmd.Process.Signal(s)
-					if err != nil {
-						panic(err)
-					}
-				}()
-
-				systemOut.Printf("Running [%s] at %s\n", c.Name, startTime.Format(time.Kitchen))
-				cmd.Run()
-
-				endTime := time.Now()
-				systemOut.Printf("Completed [%s] at %s after %s\n", c.Name, endTime.Format(time.Kitchen), endTime.Sub(startTime))
-			},
+			Run: c.Run,
 		})
 	}
+
+	rootCmd.AddCommand(devCmd)
 
 	_ = rootCmd.Execute()
 }
 
-func initConfig() *CmdConfig {
-	viper.SetConfigName(".devcontainer/cmd")
+func loadConfig() *CmdConfig {
 	viper.AddConfigPath(".")
+	viper.SetConfigType("yaml")
+	viper.SetConfigName(".cmd")
+
 	err := viper.ReadInConfig()
+
+	var x viper.ConfigFileNotFoundError
+	if errors.As(err, &x) {
+		systemErr.Printf("Config file not found: .cmd.yaml")
+		os.Exit(1)
+	}
 	if err != nil {
 		panic(err)
 	}
 
 	c := CmdConfig{
 		DevContainerName: viper.GetString("devcontainer.name"),
+		DevContainerDir:  viper.GetString("devcontainer.dir"),
 		Env:              viper.GetStringMapString("env"),
 		Commands:         make(map[string]*CmdCommand),
 	}
