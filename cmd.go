f451bc6809e96f39a9d36b226a3e36a551b83af3
bvanvugt <1531419+bvanvugt@users.noreply.github.com>
2023-03-10 21:35:31 +0000

    Rewrite.

--- cmd.go
+++ cmd.go
@@ -1,166 +1,30 @@
 package main
 
 import (
-	"context"
-	"embed"
 	"errors"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"os/signal"
-	"strings"
+	"path"
 	"syscall"
 	"time"
 
 	"github.com/fatih/color"
-	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-//go:embed files/*
-var fs embed.FS
+const DOCKER_PATH = "/usr/local/bin/docker"
 
-var devContainerCmd string
-
-var systemOut, systemErr *color.Color
-
-func init() {
-	systemOut = color.New(color.FgHiBlack)
-	systemErr = color.New(color.FgHiRed)
-}
-
-type CmdConfig struct {
+type Config struct {
 	DevContainerName string
 	DevContainerDir  string
 	Env              map[string]string
-	Commands         []*CmdCommand
+	Commands         map[string]string
 }
 
-type CmdCommand struct {
-	Name  string
-	Shell string
-}
-
-func (cc *CmdCommand) Run(_ *cobra.Command, args []string) {
-	ctx := context.Background()
-
-	startTime := time.Now()
-
-	shellString := strings.Replace(cc.Shell, "$@", strings.Join(args, " "), -1)
-	for i, arg := range args {
-		shellString = strings.Replace(shellString, fmt.Sprintf("$%d", i+1), arg, -1)
-		systemOut.Println(arg)
-	}
-
-	if len(devContainerCmd) > 0 {
-		shellString = fmt.Sprintf("%s %s", devContainerCmd, shellString)
-	}
-
-	cmd := exec.CommandContext(ctx, "bash", "-c", shellString)
-
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
-		s := <-c
-
-		systemOut.Printf("\nReceived signal: %s\n", s)
-		err := cmd.Process.Signal(s)
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	systemOut.Printf("Running [%s] at %s\n", cc.Name, startTime.Format(time.Kitchen))
-	systemOut.Println(shellString)
-
-	err := cmd.Run()
-	if err != nil {
-		panic(err)
-	}
-
-	endTime := time.Now()
-	systemOut.Printf("Completed [%s] at %s after %s\n", cc.Name, endTime.Format(time.Kitchen), endTime.Sub(startTime))
-}
-
-func writeFile(srcPath string, dstPath string) {
-	if _, err := os.Stat(dstPath); err == nil {
-		systemErr.Printf("File %s already exists\n", dstPath)
-		return
-	}
-
-	srcData, err := fs.ReadFile(srcPath)
-	if err != nil {
-		panic(err)
-	}
-
-	err = ioutil.WriteFile(dstPath, srcData, 0644)
-	if err != nil {
-		panic(err)
-	}
-
-	systemOut.Printf("Created file: %s\n", dstPath)
-}
-
-func main() {
-
-	config := loadConfig()
-
-	rootCmd := &cobra.Command{
-		Use: "cmd",
-	}
-
-	initCmd := &cobra.Command{
-		Use: "init",
-		Run: func(cmd *cobra.Command, args []string) {
-			_ = os.Mkdir(".devcontainer", os.ModePerm)
-			writeFile("files/cmd.yaml", ".devcontainer/cmd.yaml")
-		},
-	}
-	initCmd.AddCommand(&cobra.Command{
-		Use: "go",
-		Run: func(cmd *cobra.Command, args []string) {
-			_ = os.Mkdir(".devcontainer", os.ModePerm)
-			writeFile("files/go/cmd.yaml", ".devcontainer/cmd.yaml")
-			writeFile("files/go/devcontainer.json", ".devcontainer/devcontainer.json")
-			writeFile("files/go/devcontainer.env", ".devcontainer/devcontainer.env")
-			writeFile("files/go/devcontainer.sh", ".devcontainer/devcontainer.sh")
-		},
-	})
-
-	devCmd := &cobra.Command{
-		Use: "dev",
-		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			devContainerCmd = fmt.Sprintf("docker exec -it -w %s %s", config.DevContainerDir, config.DevContainerName)
-		},
-		Run: func(cmd *cobra.Command, args []string) {
-			systemErr.Println("no command name provided")
-		},
-	}
-
-	for _, c := range config.Commands {
-		devCmd.AddCommand(&cobra.Command{
-			Use: c.Name,
-			Run: c.Run,
-		})
-		rootCmd.AddCommand(&cobra.Command{
-			Use: c.Name,
-			Run: c.Run,
-		})
-	}
-
-	rootCmd.AddCommand(devCmd)
-	rootCmd.AddCommand(initCmd)
-
-	_ = rootCmd.Execute()
-}
-
-func loadConfig() *CmdConfig {
-	c := CmdConfig{}
+func loadConfig() *Config {
+	c := Config{}
 
 	viper.AddConfigPath(".devcontainer")
 	viper.SetConfigName("cmd")
@@ -180,14 +44,109 @@ func loadConfig() *CmdConfig {
 	c.DevContainerName = viper.GetString("devcontainer.name")
 	c.DevContainerDir = viper.GetString("devcontainer.dir")
 	c.Env = viper.GetStringMapString("env")
-
-	commands := viper.GetStringMapString("commands")
-	for name, command := range commands {
-		c.Commands = append(c.Commands, &CmdCommand{
-			Name:  name,
-			Shell: command,
-		})
-	}
+	c.Commands = viper.GetStringMapString("commands")
 
 	return &c
+}
+
+func main() {
+	runInDevContainer := false
+
+	systemOut := color.New(color.FgHiBlack)
+	systemErr := color.New(color.FgHiRed)
+
+	config := loadConfig()
+
+	// Does command exist?
+	cmdArg := os.Args[1]
+	cmdArgs := os.Args[2:]
+	if cmdArg == "dev" {
+		runInDevContainer = true
+		cmdArg = os.Args[2]
+		cmdArgs = os.Args[3:]
+	}
+
+	cmdStr, found := config.Commands[cmdArg]
+	if !found {
+		systemErr.Println("command not found")
+		return
+	}
+
+	// Create temp .sh file
+	f, err := ioutil.TempFile(".devcontainer", "cmd.*.sh")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(f.Name())
+
+	// Make it executable
+	err = os.Chmod(f.Name(), 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	// Write command to file
+	_, err = f.WriteString("#!/bin/bash\n\n" + cmdStr + "\n")
+	if err != nil {
+		panic(err)
+	}
+
+	// Build command to run
+	var cmd exec.Cmd
+	if runInDevContainer {
+
+		// Build docker exec command
+		if len(config.DevContainerName) <= 0 {
+			systemErr.Println("cmd.yaml: devcontainer.name not specified")
+		}
+
+		args := []string{"exec", "-i"}
+		if len(config.DevContainerDir) > 0 {
+			args = append(args, "-w", config.DevContainerDir)
+		}
+		args = append(
+			args,
+			config.DevContainerName,
+			".devcontainer/"+path.Base(f.Name()))
+		args = append(args, cmdArgs...)
+
+		cmd = exec.Cmd{
+			Path: DOCKER_PATH,
+			Args: append([]string{DOCKER_PATH}, args...),
+		}
+	} else {
+		// Build local bash command
+		cmd = exec.Cmd{
+			Path: f.Name(),
+			Args: append([]string{f.Name()}, cmdArgs...),
+		}
+
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Start listening for signals
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		s := <-c
+
+		err := cmd.Process.Signal(s)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	// Run it
+	startTime := time.Now()
+	systemOut.Printf("Running [%s] at %s\n", cmdArg, startTime.Format(time.Kitchen))
+	systemOut.Println(cmd.String())
+	err = cmd.Run()
+	if err != nil {
+		// Interrupted -- do we care?
+		systemOut.Println(err)
+		// log.Fatal(err)
+	}
+	endTime := time.Now()
+	systemOut.Printf("Completed [%s] at %s after %s\n", cmdArg, endTime.Format(time.Kitchen), endTime.Sub(startTime))
 }
